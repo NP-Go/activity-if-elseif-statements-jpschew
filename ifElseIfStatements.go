5041f81a361f7df4610f83eb2c90f82afda2b903
jpschew <jps.chew@gmail.com>
2022-03-30 09:51:40 +0800

    added code to test if a number is odd or even

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -1,6 +1,27 @@
 package main
 
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
+
 func main() {
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
+	if number1 := 17; number1%2 == 0 {
+		fmt.Println(strconv.Itoa(number1) + " is even.")
+		if math.Abs(float64(number1)) < 9 {
+			fmt.Println(strconv.Itoa(number1) + " has one digit.")
+		} else {
+			fmt.Println(strconv.Itoa(number1) + " has more than one digit.")
+		}
+	} else {
+		fmt.Println(strconv.Itoa(number1) + " is odd.")
+		if math.Abs(float64(number1)) < 9 {
+			fmt.Println(strconv.Itoa(number1) + " has one digit.")
+		} else {
+			fmt.Println(strconv.Itoa(number1) + " has more than one digit.")
+		}
+	}
 }
